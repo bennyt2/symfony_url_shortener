c7b3e0316e49f421be5e36da28fa37c76efe7e08
Ben Thomas <bennyt9000@gmail.com>
2020-08-01 16:35:09 -0400

    Create Redirect entity and persist schema to DB

---
+++ src/Entity/Redirect.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Redirect
+ *
+ * @ORM\Table(name="redirect", indexes={@ORM\Index(name="slug_idx", columns={"slug"})})
+ * @ORM\Entity()
+ */
+class Redirect
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    protected int $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="slug", type="string", length=9, nullable=false)
+     */
+    protected string $slug;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="text", nullable=false)
+     */
+    protected string $url;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @param string $slug
+     */
+    public function setSlug(string $slug): void
+    {
+        $this->slug = $slug;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     */
+    public function setUrl(string $url): void
+    {
+        $this->url = $url;
+    }
+}
