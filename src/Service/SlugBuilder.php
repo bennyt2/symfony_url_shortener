0bb0cd62b05119e7fad45570377a3291ed4a8e74
Ben Thomas <bennyt9000@gmail.com>
2020-08-01 22:07:36 -0400

    Ensure auto-generated slug is unique

--- src/Service/SlugBuilder.php
+++ src/Service/SlugBuilder.php
@@ -2,20 +2,39 @@
 
 namespace App\Service;
 
+use App\Entity\Redirect;
+use Doctrine\ORM\EntityManagerInterface;
+
 class SlugBuilder
 {
+    /** @var EntityManagerInterface */
+    protected $em;
+
+    /**
+     * SlugBuilder constructor.
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     /**
      * @param int|null $length
      * @return string
      */
     public function createSlug(?int $length = 6): string
     {
-        // No vowels to prevent curse words.
-        $chars = '123456789bcdfghjklmnpqrstvwxyz';
-        $slug = '';
-        for ($i = 0; $i < $length; $i++) {
-            $slug .= $chars[rand(0, strlen($chars)-1)];
-        }
+        $repo = $this->em->getRepository(Redirect::class);
+
+        do {
+            // No vowels to prevent curse words.
+            $chars = '123456789bcdfghjklmnpqrstvwxyz';
+            $slug = '';
+            for ($i = 0; $i < $length; $i++) {
+                $slug .= $chars[rand(0, strlen($chars)-1)];
+            }
+        } while ($repo->findOneBySlug($slug));
 
         return $slug;
     }
